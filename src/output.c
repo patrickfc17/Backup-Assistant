d71fb27c3716ddb644fd8be197979327e1b4b91e
Patrick Ferreira Carvalho <patrickferreiraC911@gmail.com>
2024-07-08 01:51:33 -0300

    feat: Beautify The Welcome Message :lipstick:

--- src/output.c
+++ src/output.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "../include/terminal.h"
 #include "../include/output.h"
@@ -16,18 +17,27 @@ void welcome()
     terminal_sizes tsizes = get_terminal_dimensions(true);
 
     char *user = getenv("USER") ? getenv("USER") : "There";
-    const char template_message[] = "-- Hello %s! --";
+    const char template_message[] = "Hello %s!";
+
+    const uint8_t LEFT_SPACE = 2;
+    const char COVER_SPECIAL_CHAR[] = "-";
 
     const size_t length = strlen(template_message) + strlen(user) + 1;
-    char hello_message[length];
+    uint16_t spacing = (tsizes.twidth - length) / 2 - 1;
 
+    char hello_message[length];
     sprintf(hello_message, template_message, user);
 
-    __uint16_t spacing = (tsizes.twidth - length) / 2;
+    printf("\033[32m");
 
-    repeat(" ", spacing);
-    printf("%s", hello_message);
-    repeat(" ", spacing);
+    repeat(COVER_SPECIAL_CHAR, tsizes.twidth - LEFT_SPACE);
+    printf("\n");
 
+    repeat("* ", spacing / 2);
+    printf("\033[34m%s\033[32m", hello_message);
+    repeat(" *", spacing / 2 + LEFT_SPACE);
+    printf("\n");
+
+    repeat(COVER_SPECIAL_CHAR, tsizes.twidth - LEFT_SPACE);
     printf("\n");
 }
